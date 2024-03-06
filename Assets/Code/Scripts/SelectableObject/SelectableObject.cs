701670f9b91984cacccb354de997a3778ff212ff
bukasker <kamil.buk.01@gmail.com>
2024-03-06 12:00:45 +0100

    Added HoverObjectScirpt and Selectable object Class

---
+++ Assets/Code/Scripts/SelectableObject/SelectableObject.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New SelectableObject", menuName = "Selection/SelectableObject")]
+public class SelectableObject : ScriptableObject
+{
+	[Header("Selectable Object Settings")]
+	public string ObjectName = "New Object";
+	public SelectableObjectType ObjectType;
+	public int WoodCapacity;
+	public int MaxWoodCapacity;
+	public int StoneCapacity;
+	public int MaxStoneCapacity;
+	public int FoodCapacity;
+	public int MaxFoodCapacity;
+	public int SettlerCapacity;
+	public int MaxSettlerCapacity;
+	public GameObject ObjectPrefab;
+
+	[TextArea]
+	public string Description = "Description placeholder";
+}
+
+public enum SelectableObjectType
+{
+	Tree,
+	CutedTree,
+	Stone,
+	MinedStone,
+	Settler,
+	Food,
+	Structure
+}
---
+++ Assets/Code/Scripts/SelectableObject/SelectableObjectManager.cs
@@ -0,0 +1,70 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[System.Serializable]
+public class SelectableObjectEvent : UnityEvent<SelectableObject> { }
+
+public class SelectableObjectManager : MonoBehaviour
+{
+	public static SelectableObjectManager Instance;
+
+	private GameObject selectedGameObject;
+	private SelectableObject selectedObject;
+
+	[Header("Select Keys")]
+	public KeyCode SelectKey = KeyCode.Mouse0;
+
+	[Header("Tree pivot")]
+	public float TreePivoitOffset = 0.8f;
+
+	[Header("Display Events")]
+	public SelectableObjectEvent StartDisplay; // Zmiana typu zdarzenia
+	public UnityEvent StopDisplay;
+
+	private void Awake()
+	{
+		if (Instance == null)
+		{
+			Instance = this;
+		}
+	}
+
+	private void Update()
+	{
+		if (Input.GetKey(SelectKey))
+		{
+			if (selectedGameObject != null)
+			{
+				MoveObjectToCursor();
+				StopDisplay.Invoke();
+			}
+		}
+	}
+
+	private void MoveObjectToCursor()
+	{
+		Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		mousePosition.z = 0f;
+		mousePosition.y -= TreePivoitOffset;
+		selectedGameObject.transform.position = mousePosition;
+	}
+
+	public void SelectObject(GameObject gameObj)
+	{
+		selectedGameObject = gameObj;
+		var hoverObject = selectedGameObject.GetComponent<HoverObject>();
+		selectedObject = hoverObject.ObjectToSelect;
+		if (!Input.GetKey(SelectKey))
+		{
+			StartDisplay?.Invoke(selectedObject);
+		}
+	}
+
+	public void RemoveSelectedObject()
+	{
+		selectedGameObject = null;
+		selectedObject = null;
+
+		StopDisplay.Invoke();
+	}
+}
