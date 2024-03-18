338088bf43744d40df1e1cf5a7e60d3c605499e0
Przemysław Bujok <przemyslaw.bujok@gmail.com>
2024-03-18 19:16:52 +0100

    Button click animation script added

--- Assets/UI/Scripts/ButtonClickAnimation.cs
+++ Assets/UI/Scripts/ButtonClickAnimation.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ButtonClickAnimation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Button Settings")]
+    [SerializeField] private Button button;
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite clickedSprite;
+
+    [Header("Audio Settings")]
+    [SerializeField] private AudioClip clickSound;
+    [SerializeField] private AudioSource audioSource;
+
+    private Sprite originalSprite;
+
+    private void Start()
     {
-        
+        originalSprite = button.image.sprite;
+        button.onClick.AddListener(OnClick);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnClick()
     {
-        
+        button.image.sprite = clickedSprite;
+        PlayClickSound();
+        StartCoroutine(ResetButtonSprite());
+    }
+
+    private System.Collections.IEnumerator ResetButtonSprite()
+    {
+        yield return new WaitForSeconds(0.5f);
+        button.image.sprite = originalSprite;
+    }
+
+    private void PlayClickSound()
+    {
+        audioSource.PlayOneShot(clickSound);
     }
 }
